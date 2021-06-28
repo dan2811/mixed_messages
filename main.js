37c547944505114ead55534a2e2ed86d4a1fb9ad
dan2811 <djordandrums@gmail.com>
2021-06-28 14:53:19 +0100

    Take prompt and display string result in console.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const prompt = require('prompt-sync')({sigint: true});
 
 const locations = ['Barking Abbey', 'the Olympic Park', 'Hyde Park', 'Barking Park'];
 const times = ['15 minutes', '30 minutes', '45 minutes', '1 hour'];
@@ -8,6 +8,6 @@ let randPlace = locations[Math.floor(Math.random()*4)];
 let randTime = times[Math.floor(Math.random()*4)];
 let randToy = toys[Math.floor(Math.random()*4)];
 
+let dogName = prompt("Enter your dog's name:  ");
 
-//Testing random generators.
-console.log(randPlace, randTime, randToy);
+console.log(`${dogName} should be taken to ${randPlace} today for ${randTime}. Don't forget to take the ${randToy}!`);
